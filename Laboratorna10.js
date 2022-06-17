6842548f974296b1f2c505be6a63d8e8d3163c5e
ТЗ-02RemesloMisha <105556220+unitteam1@users.noreply.github.com>
2022-06-17 15:04:27 +0300

    Add files via upload

---
+++ Laboratorna10.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+$("#modal-btn").click(function()
+{
+    $("#my-modal").modal();
+});
+
+$(".button1").click(function()
+{
+    $("#my-modal1").modal();
+});
+  
+$(".button2").click(function()
+{
+    $("#my-modal2").modal();
+});
+  
+  $(".button3").click(function()
+{
+    $("#my-modal3").modal();
+});
+  $(".button4").click(function()
+{
+    $("#my-modal4").modal();
+});
+  
+  
